1e184da10355a46620fa392a226aedb5f5107fbb
Jtljac <jtljac2@gmail.com>
2024-09-01 14:03:28 +0100

    Improve Nav bar for mobile

--- themes/hackspace-theme/assets/js/main.js
+++ themes/hackspace-theme/assets/js/main.js
@@ -1,19 +1,16 @@
 /**
  * Toggle the navigation dropdown
  */
-function toggleNavDropdown() {
+function toggleNavDropdown(navButton) {
     const dropDown = document.querySelector("#nav-wrap");
 
     if (!dropDown) throw new Error("Failed to find navbar");
-    if (dropDown.classList.contains("nav-toggled")) {
-        dropDown.classList.remove("nav-toggled");
-    } else {
-        dropDown.classList.add("nav-toggled");
-    }
+    dropDown.classList.toggle("nav-toggled");
+    navButton.classList.toggle("nav-toggled");
 }
 
 function registerNav() {
-    document.querySelector("#nav-mobile svg:last-child")?.addEventListener("click", (event) => {
-        toggleNavDropdown();
+    document.querySelector("#nav-mobile .nav-hamburger")?.addEventListener("click", (event) => {
+        toggleNavDropdown(event.currentTarget);
     });
 }
