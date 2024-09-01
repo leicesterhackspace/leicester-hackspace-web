6654951ed9d8d86937e9d643bc5587ec2ff161dc
Jtljac <jtljac2@gmail.com>
2024-09-02 00:26:55 +0100

    Add modal for viewing full screen images

--- themes/hackspace-theme/assets/js/main.js
+++ themes/hackspace-theme/assets/js/main.js
@@ -9,8 +9,39 @@ function toggleNavDropdown(navButton) {
     navButton.classList.toggle("nav-toggled");
 }
 
-function registerNav() {
+window.addEventListener("load", () => {
     document.querySelector("#nav-mobile .nav-hamburger")?.addEventListener("click", (event) => {
         toggleNavDropdown(event.currentTarget);
     });
+});
+
+/**
+ * Open the modal dialog with an image
+ * @param src The path to the image to open in the modal
+ */
+function spawnModal(src) {
+    const modal = document.querySelector("#image-modal");
+
+    const preload = new Image();
+    preload.src = src;
+    preload.addEventListener("load", () => {
+        modal.querySelector("img").src = preload.src;
+        modal.showModal();
+    })
 }
+
+window.addEventListener("load", () => {
+    const modal = document.querySelector("#image-modal");
+    modal.querySelector("button").addEventListener("click", () => modal.close());
+
+    modal.addEventListener("click", (event) => {
+        if (event.target === modal) modal.close()
+    });
+
+    document.querySelectorAll("picture[data-large]")
+        .forEach(picture => {
+            picture.addEventListener("click", () => {
+                spawnModal(picture.dataset.large);
+            })
+        });
+})
